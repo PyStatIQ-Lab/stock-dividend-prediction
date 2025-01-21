d658fc0cb2f866d1158f663311ee5e3ca83fd465
PyStatIQ-Lab <support@pystatiq.com>
2025-01-21 13:36:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ st.markdown(f"""
             display: block;
             margin-left: auto;
             margin-right: auto;
-            width: 35px;
+            width: 90px;
             padding-top: 30px;
         }}
     </style>
